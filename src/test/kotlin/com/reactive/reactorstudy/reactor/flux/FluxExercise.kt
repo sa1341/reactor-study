39a8b5dae2a13118a250eb0113a10162dc570ac0
임준영 <a79007714@gmail.com>
2023-06-15 23:49:48 +0900

    study: reactor flux error handling 예제코드 작성

--- src/test/kotlin/com/reactive/reactorstudy/reactor/flux/FluxExercise.kt
+++ src/test/kotlin/com/reactive/reactorstudy/reactor/flux/FluxExercise.kt
@@ -3,11 +3,34 @@ package com.reactive.reactorstudy.reactor.flux
 import mu.KotlinLogging
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 
 private val log = KotlinLogging.logger {}
 
 class FluxExercise {
+
+    @Test
+    fun concatTest() {
+        // given
+        val flux = Mono.justOrEmpty("jean")
+            .concatWith(Mono.justOrEmpty("calm"))
+
+        // when
+        flux.subscribe(log::info)
+    }
+
+    @Test
+    fun concatToMono() {
+        // given
+        Flux.concat(
+            Flux.just("Mercury", "Venus", "Earth"),
+            Flux.just("Mars", "Jupiter", "Saturn"),
+            Flux.just("Uranus", "Neptune", "Pluto")
+        ).collectList().subscribe {
+            log.info { "$it" }
+        }
+    }
 
     @Test
     fun doOnNextTest() {
@@ -24,5 +47,21 @@ class FluxExercise {
         }.subscribe {
             log.debug { "value: $it" }
         }
+    }
+
+    @Test
+    fun onErrorResumeTest() {
+        // given
+        Flux.just(1)
+            .onErrorResume {
+                Mono.just<Int>(shouldDoOnError(it))
+            }.subscribe {
+                log.debug { "value: $it" }
+            }
+    }
+
+    private fun shouldDoOnError(t: Throwable): Int {
+        log.error { "ERROR: $t" }
+        return 1
     }
 }
