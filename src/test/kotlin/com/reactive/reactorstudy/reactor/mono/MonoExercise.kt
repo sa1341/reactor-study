87a00761a0667f35d3adcb7ec0a6f39e766abe1c
임준영 <a79007714@gmail.com>
2023-06-19 23:38:49 +0900

    study: reactor context API를 사용하여 각 스레드별로 공통의 컨텍스트 제공이 가능한 것을 예제코드로 학습함
    1. Context는 구독이 발생할 때마다 하나의 Context가 해당 구독에 연결됩니다.
    2. Context는 Operator 체인의 아래에서 위로 전파됩니다.

--- src/test/kotlin/com/reactive/reactorstudy/reactor/mono/MonoExercise.kt
+++ src/test/kotlin/com/reactive/reactorstudy/reactor/mono/MonoExercise.kt
@@ -3,8 +3,11 @@ package com.reactive.reactorstudy.reactor.mono
 import mu.KotlinLogging
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Mono
+import reactor.util.context.Context
+import reactor.util.function.Tuple2
 
 private val log = KotlinLogging.logger {}
+const val HEADER_AUTH_TOKEN = "authorization"
 
 class MonoExercise {
 
@@ -17,4 +20,44 @@ class MonoExercise {
             log.debug { "next: $it" }
         }
     }
+
+    @Test
+    fun contextTest() {
+        // given
+        val mono = postBook(
+            Mono.just(
+                Book(
+                    phoneNumber = "010-7900-7714",
+                    name = "book",
+                    author = "junyoung"
+                )
+            )
+        ).contextWrite(Context.of(HEADER_AUTH_TOKEN, "eyJhGciOi"))
+
+        // when
+        mono.subscribe {
+            log.info { "# onNext: $it" }
+        }
+    }
+
+    private fun postBook(book: Mono<Book>): Mono<String> {
+        val zip: Mono<Tuple2<Book, String>> = Mono
+            .zip(
+                book,
+                Mono.deferContextual {
+                    Mono.just(it.get(HEADER_AUTH_TOKEN))
+                }
+            )
+
+        return zip.flatMap {
+            val response = "POST the book(${it.t1.name}, ${it.t1.author}) with token: ${it.t2}"
+            Mono.just(response)
+        }
+    }
 }
+
+data class Book(
+    val phoneNumber: String,
+    val name: String,
+    val author: String
+)
