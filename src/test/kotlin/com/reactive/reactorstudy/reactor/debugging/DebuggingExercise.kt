e511f699083ff8c5b72913f0a737be1b6ba5cc1f
임준영 <a79007714@gmail.com>
2023-06-23 23:51:07 +0900

    study: reactor debugging 모드 활성화를 통해서 비동기로 operator 체인 실행 시 디버깅하는 예제코드 작성

---
+++ src/test/kotlin/com/reactive/reactorstudy/reactor/debugging/DebuggingExercise.kt
@@ -0,0 +1,67 @@
+package com.reactive.reactorstudy.reactor.debugging
+
+import mu.KotlinLogging
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Hooks
+import reactor.core.scheduler.Schedulers
+import java.util.concurrent.TimeUnit
+
+private val log = KotlinLogging.logger {}
+
+class DebuggingExercise {
+
+    private val fruits: Map<String, String> = createFruits()
+
+    @Test
+    fun debugTest() {
+        Hooks.onOperatorDebug()
+
+        // given
+        Flux.fromArray(arrayOf("BANANAS", "APPLES", "PEARS", "MELONS"))
+            .subscribeOn(Schedulers.boundedElastic())
+            .publishOn(Schedulers.parallel())
+            .map(String::lowercase)
+            .map { it.substring(0, it.length - 1) }
+            .map(fruits::get)
+            .map { "맛있는 $it" }
+            .subscribe(
+                {
+                    log.info { it }
+                },
+                {
+                    log.error { "# onError: $it" }
+                }
+            )
+
+        TimeUnit.MILLISECONDS.sleep(100)
+    }
+
+    @Test
+    fun checkPointTest() {
+        // given
+        Flux.just(2, 4, 6, 8)
+            .zipWith(Flux.just(1, 2, 3, 0)) { a: Int, b: Int -> a / b }
+            .map { it + 2 }
+            .checkpoint()
+            .subscribe(
+                {
+                    log.info { "# onNext: $it" }
+                },
+                {
+                    log.error { log.error { "# onError: $it" } }
+                }
+            )
+    }
+
+    companion object {
+        fun createFruits(): Map<String, String> {
+            val fruits = mutableMapOf<String, String>()
+            fruits["banana"] = "바나나"
+            fruits["apple"] = "사과"
+            fruits["pear"] = "배"
+            fruits["grape"] = "포도"
+            return fruits
+        }
+    }
+}
