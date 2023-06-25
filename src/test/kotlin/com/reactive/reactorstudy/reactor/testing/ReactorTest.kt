cd980534efdb5022bacf5d3ad6eeff5b215bafdc
임준영 <a79007714@gmail.com>
2023-06-25 21:48:10 +0900

    study: reactor recordWith, PublisherProbe를 사용하여 더 상세하게 reactor streams 사양과 관련하여 테스트를 용이하게 작성할 수 있는 테스트 예제코드를 작성하였음.

--- src/test/kotlin/com/reactive/reactorstudy/reactor/testing/ReactorTest.kt
+++ src/test/kotlin/com/reactive/reactorstudy/reactor/testing/ReactorTest.kt
@@ -1,9 +1,13 @@
 package com.reactive.reactorstudy.reactor.testing
 
 import org.junit.jupiter.api.Test
+import org.springframework.util.Base64Utils
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 import reactor.test.StepVerifierOptions
+import reactor.test.publisher.PublisherProbe
+import reactor.test.publisher.TestPublisher
 import reactor.test.scheduler.VirtualTimeScheduler
 import reactor.util.function.Tuple2
 import reactor.util.function.Tuples
@@ -58,6 +62,84 @@ class ReactorTest {
             .verify()
     }
 
+    @Test
+    fun contextTest() {
+        // given
+        val source = Mono.just("hello")
+
+        StepVerifier
+            .create(
+                getSecretMessage(source)
+                    .contextWrite { it.put("secretMessage", "Hello, Reactor") }
+                    .contextWrite { it.put("secretKey", "aGVsbG8") }
+            )
+            .expectSubscription()
+            .expectAccessibleContext()
+            .hasKey("secretKey")
+            .hasKey("secretMessage")
+            .then()
+            .expectNext("Hello, Reactor")
+            .expectComplete()
+            .verify()
+    }
+
+    @Test
+    fun recordWithTest() {
+        // given
+        StepVerifier
+            .create(
+                getCapitalizedCountry(
+                    Flux.just("korea", "england", "canada", "india")
+                )
+            )
+            .expectSubscription()
+            .recordWith {
+                arrayListOf()
+            }
+            .thenConsumeWhile {
+                it.isNotEmpty()
+            }
+            .expectRecordedMatches {
+                it.stream()
+                    .allMatch {
+                        Character.isUpperCase(it[0])
+                    }
+            }
+            .expectComplete()
+            .verify()
+    }
+
+    @Test
+    fun testPublisherTest() {
+        // given
+        val source = TestPublisher.create<Int>()
+
+        // when
+        StepVerifier
+            .create(divideByTwo(source.flux()))
+            .expectSubscription()
+            .then {
+                source.emit(2, 4, 6, 8, 10)
+            }
+            .expectNext(1, 2, 3, 4)
+            .expectError()
+            .verify()
+    }
+
+    @Test
+    fun publisherProbeTest() {
+        // given
+        val probe = PublisherProbe.of(supplyStandbyPower())
+
+        // when
+        StepVerifier
+            .create(
+                processTask(supplyMainPower(), probe.mono())
+            )
+            .expectNextCount(1)
+            .verifyComplete()
+    }
+
     companion object {
         fun divideByTwo(source: Flux<Int>) =
             source.zipWith(
@@ -84,5 +166,36 @@ class ReactorTest {
                     )
                 }
         }
+
+        fun getSecretMessage(keySource: Mono<String>): Mono<String> {
+            return keySource
+                .zipWith(
+                    Mono.deferContextual {
+                        Mono.just(it.get("secretKey") as String)
+                    }
+                )
+                .filter {
+                    it.t1 == String(Base64Utils.decodeFromString(it.t2))
+                }
+                .transformDeferredContextual { mono, ctx ->
+                    mono.map { ctx.get("secretMessage") }
+                }
+        }
+
+        fun getCapitalizedCountry(source: Flux<String>): Flux<String> {
+            return source.map {
+                "${it.substring(0, 1).uppercase()}${it.substring(1)}"
+            }
+        }
+
+        fun processTask(main: Mono<String>, standby: Mono<String>): Mono<String> {
+            return main.flatMap {
+                Mono.just(it)
+            }.switchIfEmpty(standby)
+        }
+
+        fun supplyMainPower() = Mono.empty<String>()
+
+        fun supplyStandbyPower() = Mono.just("# supply Standby Power")
     }
 }
